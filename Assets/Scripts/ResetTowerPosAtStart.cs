c33962a60ca056b71891e45e620526892ac16407
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-12-13 22:40:05 -0600

    reset tower pos to ground

---
+++ Assets/Scripts/ResetTowerPosAtStart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ResetTowerPosAtStart : MonoBehaviour
+{
+
+    [SerializeField] LayerMask groundLayerMask;
+
+    void Start()
+    {
+        RaycastHit groundHit;
+        Ray ray = new Ray();
+        ray.direction = Vector3.down;
+        ray.origin = transform.position;
+        float towerYExtent = transform.Find("GFX").GetComponent<Renderer>().bounds.extents.y;
+        if (Physics.Raycast(ray, out groundHit, Mathf.Infinity, groundLayerMask))
+        {
+            transform.position = groundHit.point + Vector3.up * towerYExtent;
+        }
+    }
+
+}
