1805376c1b4d751d85d17f7eb016c32607cbf500
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-12-15 14:44:38 -0600

    created new castle for end point

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -5,15 +5,28 @@ public class EnemyGenerator : MonoBehaviour
 
     [SerializeField] float timeBetweenSpawns = 5f;
     [SerializeField] GameObject enemyPrefab;
+    [SerializeField] LayerMask groundLayerMask;
 
     float timeSinceLastSpawn = 0f;
+    Vector3 spawnPoint;
 
     void Start()
     {
         Transform pathNodesParent = GameObject.Find("Path Nodes").transform;
         foreach (Transform path in pathNodesParent)
         {
-            transform.position = path.position;
+            RaycastHit groundHit;
+            Ray ray = new Ray();
+            ray.direction = Vector3.down;
+            ray.origin = path.position;
+            if (Physics.Raycast(ray, out groundHit, Mathf.Infinity, groundLayerMask))
+            {
+                Vector3 pos = transform.position;
+                pos = groundHit.point;
+                spawnPoint = pos;
+                pos.y += GetComponent<Collider>().bounds.extents.y;
+                transform.position = pos;
+            }
             break;
         }
     }
@@ -25,9 +38,9 @@ public class EnemyGenerator : MonoBehaviour
         if (timeSinceLastSpawn > timeBetweenSpawns)
         {
             timeSinceLastSpawn = 0f;
-            GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            GameObject enemy = Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
             Vector3 pos = enemy.transform.position;
-            pos.y += enemy.transform.Find("GFX").GetComponent<Renderer>().bounds.extents.y - transform.Find("GFX").GetComponent<Renderer>().bounds.extents.y;
+            pos.y += enemy.GetComponent<Collider>().bounds.extents.y;
             enemy.transform.position = pos;
         }
     }
--- Assets/Scripts/GoToPathEnd.cs
+++ Assets/Scripts/GoToPathEnd.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class GoToPathEnd : MonoBehaviour
 {
 
+    [SerializeField] new Collider collider;
+    [SerializeField] LayerMask groundLayerMask;
+    [SerializeField] Transform doorTransform;
+
     void Start()
     {
         Transform pathNodesParent = GameObject.Find("Path Nodes").transform;
         Transform prev = transform;
+        Transform last = transform;
         foreach (Transform path in pathNodesParent)
         {
-            prev = path;
+            prev = last;
+            last = path;
         }
-        transform.position = prev.position;
+        RaycastHit groundHit;
+        Ray ray = new Ray();
+        ray.direction = Vector3.down;
+        ray.origin = last.position;
+        float yExtent = collider.bounds.extents.y;
+        Vector3 transformOffset = RemoveY(doorTransform.position - transform.position);
+        if (Physics.Raycast(ray, out groundHit, Mathf.Infinity, groundLayerMask))
+        {
+            transform.position = (groundHit.point + transformOffset) + Vector3.up * yExtent;
+        }
     }
 
+    Vector3 RemoveY(Vector3 vec)
+    {
+        return Vector3.Scale(vec, new Vector3(1f, 0f, 1f));
+    }
 }
